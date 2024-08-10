a53599046f889b8bb058e453b49d82fa21d5e5fa
LeandroNapoli <leandro.napoli12@gmail.com>
2024-08-10 09:57:23 -0300

    Incluindo js para ativar o menu

---
+++ LeliAdmin/LeliAdmin.Infra/Contants/Menus.cs
@@ -0,0 +1,8 @@
+﻿namespace LeliAdmin.Infra.Contants
+{
+    public static class Menus
+    {
+        public const string Home = "home_menu";
+        public const string Privacy = "privacy_menu";
+    }
+}
--- LeliAdmin/LeliAdmin/Script/Global/navbar.js
+++ LeliAdmin/LeliAdmin/Script/Global/navbar.js
@@ -1,1 +1,12 @@
-﻿
+﻿$(document).ready(function () {
+    selecionaMenu()
+})
+
+
+function selecionaMenu() {
+    const menuSelecionado = $('#codigoMenu').val();
+    console.log = "Alo"
+    if (menuSelecionado != "") {
+        $('#' + menuSelecionado).addClass('active');
+    }
+}
